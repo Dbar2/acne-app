30444195b5be56c85292991a24da51077bb69437
David Barry <davidbarry@MacBook-Pro.local>
2024-12-12 15:03:27 -0500

    Updates

---
+++ precache-manifest.3338efc807320ceed649148bafd53902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768e5c798a96d2400c997a1d5a53f70d",
+    "url": "/acne-app/index.html"
+  },
+  {
+    "revision": "ed8155a0c159e5df7f43",
+    "url": "/acne-app/static/css/2.876f1645.chunk.css"
+  },
+  {
+    "revision": "34b63b51fd22004ab626",
+    "url": "/acne-app/static/css/main.e53086db.chunk.css"
+  },
+  {
+    "revision": "ed8155a0c159e5df7f43",
+    "url": "/acne-app/static/js/2.5373d395.chunk.js"
+  },
+  {
+    "revision": "da96256dd070ffac0081a3cd003f3cfd",
+    "url": "/acne-app/static/js/2.5373d395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b63b51fd22004ab626",
+    "url": "/acne-app/static/js/main.09775ee7.chunk.js"
+  },
+  {
+    "revision": "d5c68e328b7eaa4c724e",
+    "url": "/acne-app/static/js/runtime-main.81af167f.js"
+  }
+]);
